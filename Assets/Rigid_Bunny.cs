4bc9a1fbf989fb8a0397d680ba112a7ed57b1381
Andy <confuzzed03@gmail.com>
2016-02-28 17:46:49 -0500

    Initial Commit
    First Commit

---
+++ Assets/Rigid_Bunny.cs
@@ -0,0 +1,221 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rigid_Bunny : MonoBehaviour 
+{
+	public bool launched = false;
+	
+	public Vector3 	x;							// position
+	public Vector3 v=new Vector3(0, 0, 0);		// velocity
+	public Quaternion q = Quaternion.identity;	// quaternion
+	public Vector3 w=new Vector3(2, 0, 0);		// angular velocity
+	
+	public float m;
+	public float mass;							// mass
+	public Matrix4x4 I_body;					// body inertia
+
+	public float linear_damping;				// for damping
+	public float angular_damping;
+	public float restitution;					// for collision
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		//Initialize coefficients
+		w = new Vector3 (0, 0, 2);
+		x = new Vector3 (0, 0.6f, 0);
+		v = new Vector3 (0, 0, 0);
+		q = Quaternion.identity;
+		linear_damping  = 0.999f;
+		angular_damping = 0.98f;
+		restitution 	= 0.5f;		//elastic collision
+		m 				= 1;
+		mass 			= 0; 
+		
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		for (int i=0; i<vertices.Length; i++) 
+		{
+			mass += m;
+			float diag=m*vertices[i].sqrMagnitude;
+			I_body[0, 0]+=diag;
+			I_body[1, 1]+=diag;
+			I_body[2, 2]+=diag;
+			I_body[0, 0]-=m*vertices[i][0]*vertices[i][0];
+			I_body[0, 1]-=m*vertices[i][0]*vertices[i][1];
+			I_body[0, 2]-=m*vertices[i][0]*vertices[i][2];
+			I_body[1, 0]-=m*vertices[i][1]*vertices[i][0];
+			I_body[1, 1]-=m*vertices[i][1]*vertices[i][1];
+			I_body[1, 2]-=m*vertices[i][1]*vertices[i][2];
+			I_body[2, 0]-=m*vertices[i][2]*vertices[i][0];
+			I_body[2, 1]-=m*vertices[i][2]*vertices[i][1];
+			I_body[2, 2]-=m*vertices[i][2]*vertices[i][2];
+		}
+		I_body [3, 3] = 1;
+	}
+	
+	Matrix4x4 Get_Cross_Matrix(Vector3 a)
+	{
+		//Get the cross product matrix of vector a
+		Matrix4x4 A = Matrix4x4.zero;
+		A [0, 0] = 0; 
+		A [0, 1] = -a [2]; 
+		A [0, 2] = a [1]; 
+		A [1, 0] = a [2]; 
+		A [1, 1] = 0; 
+		A [1, 2] = -a [0]; 
+		A [2, 0] = -a [1]; 
+		A [2, 1] = a [0]; 
+		A [2, 2] = 0; 
+		A [3, 3] = 1;
+		return A;
+	}
+	
+	Matrix4x4 Quaternion_2_Matrix(Quaternion q)
+	{
+		//Get the rotation matrix R from quaternion q
+		Matrix4x4 R = Matrix4x4.zero;
+		R[0, 0]=q[3]*q[3]+q[0]*q[0]-q[1]*q[1]-q[2]*q[2];
+		R[0, 1]=2*(q[0]*q[1]-q[3]*q[2]);
+		R[0, 2]=2*(q[0]*q[2]+q[3]*q[1]);
+		R[1, 0]=2*(q[0]*q[1]+q[3]*q[2]);
+		R[1, 1]=q[3]*q[3]-q[0]*q[0]+q[1]*q[1]-q[2]*q[2];
+		R[1, 2]=2*(q[1]*q[2]-q[3]*q[0]);
+		R[2, 0]=2*(q[0]*q[2]-q[3]*q[1]);
+		R[2, 1]=2*(q[1]*q[2]+q[3]*q[0]);
+		R[2, 2]=q[3]*q[3]-q[0]*q[0]-q[1]*q[1]+q[2]*q[2];
+		R[3, 3]=1;
+		return R;
+	}
+
+	Quaternion Normalize_Quaternion(Quaternion q)
+	{
+		Quaternion result;
+		float q_length=Mathf.Sqrt(q.x*q.x+q.y*q.y+q.z*q.z+q.w*q.w);
+		result.x=q.x/q_length;
+		result.y=q.y/q_length;
+		result.z=q.z/q_length;
+		result.w=q.w/q_length;
+		return result;
+	}
+
+	Quaternion Quaternion_Multiply(Quaternion q1, Quaternion q2)
+	{
+		Vector3 v1 = new Vector3(q1.x, q1.y, q1.z);
+		Vector3 v2 = new Vector3(q2.x, q2.y, q2.z); 
+		float s1   = q1.w;
+		float s2   = q2.w;
+		Vector3 v = s1 * v2 + s2 * v1 + Vector3.Cross (v1, v2);
+		float s   = s1 * s2 - Vector3.Dot (v1, v2);
+		Quaternion q = new Quaternion (v.x, v.y, v.z, s);
+		return q;
+	}
+
+
+    void Collision_Handler(float dt)
+    {
+        Matrix4x4 R = Quaternion_2_Matrix(q);
+
+        float rest = restitution;
+
+        Vector3 N = new Vector3(0, 1, 0);
+        Vector3 r_sum = new Vector3(0, 0, 0);
+        Vector3 ri, xi, vi;
+        Vector3[] vertices = GetComponent<MeshFilter>().mesh.vertices;
+
+        float c = 0;
+
+        foreach (Vector3 i in vertices)
+        {
+            ri = R * i;
+            xi = x + ri;
+            vi = v + Vector3.Cross(w, ri);
+            if (Vector3.Dot(xi, N) < 0 && Vector3.Dot(vi, N) < 0)
+            {
+                r_sum += ri;
+                c += 1;
+            } 
+        }
+        if (c != 0)
+        {
+            ri = r_sum / c;
+            vi = v + Vector3.Cross(w, ri);
+            Matrix4x4 Ri = Get_Cross_Matrix(ri);
+            Matrix4x4 I = R * I_body * Matrix4x4.Transpose(R);
+            Matrix4x4 K = Matrix4x4.zero;
+            K.m00 = 1 / mass;
+            K.m11 = 1 / mass;
+            K.m22 = 1 / mass;
+            K.m33 = 1 / mass;
+            Matrix4x4 K1 = Ri * I.inverse * Ri;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    K[i, j] -= K1[i,j];
+                }
+            }
+            float mag = Mathf.Abs(Vector3.Dot(vi, N));
+            if (mag < 0.3)
+            {
+                rest = 0;
+            }
+            Vector3 J = K.inverse * (-vi - rest * (Vector3.Dot(vi, N)) * N);
+            v += J / mass;
+            Vector4 w1 = I.inverse * Vector3.Cross(ri, J);
+            w += new Vector3(w1.x, w1.y, w1.z);
+        }
+    }
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		float dt = 0.02f;
+
+		if(Input.GetKey("r"))
+		{
+			x = new Vector3 (0, 0.6f, 0);
+            //v = new Vector3(0, 0, 0);
+            //w = new Vector3(0, 0, 2);
+            restitution = 0.5f;
+			launched=false;
+		}
+
+		if(Input.GetKey("l"))
+		{
+			v = new Vector3 (4, 2, 0);
+			launched=true;
+		}
+
+		// Part I: Update velocities
+		if (launched)
+        {
+            v = v + dt * new Vector3(0, -9.8f, 0);
+        }
+
+        // Apply linear and angular damping
+        v = v * linear_damping;
+        w = w * angular_damping;
+
+		// Part II: Collision Handler
+		Collision_Handler (dt);
+
+        // Part III: Update position & orientation
+        //Update linear status
+        if (launched)
+        {
+            x = x + dt * v;
+        }
+        //Update angular status
+        Quaternion a = Quaternion_Multiply(new Quaternion(w.x, w.y, w.z, 0), q);
+        float c = 0.5f * dt;
+        a = new Quaternion(a.x * c, a.y * c, a.z * c, a.w * c);
+        q = Normalize_Quaternion(new Quaternion(q.x + a.x, q.y + a.y, q.z + a.z, q.w + a.w));
+
+        // Part IV: Assign to the bunny object
+        transform.position = x;
+		transform.rotation = q;
+	}
+}
---
+++ Assets/cloth_motion.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cloth_motion: MonoBehaviour {
+
+	float 		t;
+	int[] 		edge_list;
+	float 		mass;
+	float		damping;
+	float 		stiffness;
+	float[] 	L0;
+	Vector3[] 	velocities;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		t 			= 0.075f;
+		mass 		= 1.0f;
+		damping 	= 0.99f;
+		stiffness 	= 1000.0f;
+
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		int[] 		triangles = mesh.triangles;
+		Vector3[] 	vertices = mesh.vertices;
+
+		//Construct the original edge list
+		int[] original_edge_list = new int[triangles.Length*2];
+		for (int i=0; i<triangles.Length; i+=3) 
+		{
+			original_edge_list[i*2+0]=triangles[i+0];
+			original_edge_list[i*2+1]=triangles[i+1];
+			original_edge_list[i*2+2]=triangles[i+1];
+			original_edge_list[i*2+3]=triangles[i+2];
+			original_edge_list[i*2+4]=triangles[i+2];
+			original_edge_list[i*2+5]=triangles[i+0];
+		}
+		//Reorder the original edge list
+		for (int i=0; i<original_edge_list.Length; i+=2)
+			if(original_edge_list[i] > original_edge_list[i + 1]) 
+				Swap(ref original_edge_list[i], ref original_edge_list[i+1]);
+		//Sort the original edge list using quicksort
+		Quick_Sort (ref original_edge_list, 0, original_edge_list.Length/2-1);
+
+		int count = 0;
+		for (int i=0; i<original_edge_list.Length; i+=2)
+			if (i == 0 || 
+			    original_edge_list [i + 0] != original_edge_list [i - 2] ||
+			    original_edge_list [i + 1] != original_edge_list [i - 1]) 
+					count++;
+
+		edge_list = new int[count * 2];
+		int r_count = 0;
+		for (int i=0; i<original_edge_list.Length; i+=2)
+			if (i == 0 || 
+			    original_edge_list [i + 0] != original_edge_list [i - 2] ||
+				original_edge_list [i + 1] != original_edge_list [i - 1]) 
+			{
+				edge_list[r_count*2+0]=original_edge_list [i + 0];
+				edge_list[r_count*2+1]=original_edge_list [i + 1];
+				r_count++;
+			}
+
+
+		L0 = new float[edge_list.Length/2];
+		for (int e=0; e<edge_list.Length/2; e++) 
+		{
+			int v0 = edge_list[e*2+0];
+			int v1 = edge_list[e*2+1];
+			L0[e]=(vertices[v0]-vertices[v1]).magnitude;
+		}
+
+		velocities = new Vector3[vertices.Length];
+		for (int v=0; v<vertices.Length; v++)
+			velocities [v] = new Vector3 (0, 0, 0);
+
+		//for(int i=0; i<edge_list.Length/2; i++)
+		//	Debug.Log ("number"+i+" is" + edge_list [i*2] + "and"+ edge_list [i*2+1]);
+	}
+
+	void Quick_Sort(ref int[] a, int l, int r)
+	{
+		int j;
+		if(l<r)
+		{
+			j=Quick_Sort_Partition(ref a, l, r);
+			Quick_Sort (ref a, l, j-1);
+			Quick_Sort (ref a, j+1, r);
+		}
+	}
+
+	int  Quick_Sort_Partition(ref int[] a, int l, int r)
+	{
+		int pivot_0, pivot_1, i, j;
+		pivot_0 = a [l * 2 + 0];
+		pivot_1 = a [l * 2 + 1];
+		i = l;
+		j = r + 1;
+		while (true) 
+		{
+			do ++i; while( i<=r && (a[i*2]<pivot_0 || a[i*2]==pivot_0 && a[i*2+1]<=pivot_1));
+			do --j; while(  a[j*2]>pivot_0 || a[j*2]==pivot_0 && a[j*2+1]> pivot_1);
+			if(i>=j)	break;
+			Swap(ref a[i*2], ref a[j*2]);
+			Swap(ref a[i*2+1], ref a[j*2+1]);
+		}
+		Swap (ref a [l * 2 + 0], ref a [j * 2 + 0]);
+		Swap (ref a [l * 2 + 1], ref a [j * 2 + 1]);
+		return j;
+	}
+
+	void Swap(ref int a, ref int b)
+	{
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+
+
+	void Strain_Limiting()
+	{
+        foreach(int edge in edge_list)
+        {
+        }
+	}
+
+
+	void Collision_Handling()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		Vector3[] vertices = mesh.vertices;
+        for (int x = 0; x < vertices.Length; x++)
+        {
+            if(x != 0 && x != 10)
+            {
+                velocities[x] += t * new Vector3(0, -9.8f, 0);
+                velocities[x] *= damping;
+                vertices[x] += t * velocities[x];
+            }
+        }
+        mesh.vertices = vertices;
+
+		mesh.RecalculateNormals ();
+
+	}
+}
